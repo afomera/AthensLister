672c85b1e01337fba9e9b580ffec19f503a3e462
King601 <andrew@zerlex.net>
2015-05-09 23:24:44 -0500

    Gemfile and Logging Module

--- AthensLister.rb
+++ AthensLister.rb
@@ -1,3 +1,30 @@
 #!/usr/bin/env ruby
+require 'logger'
 
-puts "Hello World from AthensLister!"
+module Logging
+	def logger
+		# Control the level of logging output to console / file
+		# If silent is true, the log will NOT output Debug Messages
+		# change silent to false to output debug level messages
+		slient = false
+		if slient == false
+			Logging.logger.level = Logger::DEBUG
+		else
+			Logging.logger.level = Logger::INFO
+		end
+
+		# Format Logger Output to: [DEBUG] 04-30 22:58:36: MSG
+		Logging.logger.formatter = proc do |severity, datetime, progname, msg|
+		   "[#{severity}] #{datetime.strftime('%m-%d %H:%M:%S')}: #{msg}\n"
+		end
+		Logging.logger
+	end
+
+	def self.logger
+		@logger ||= Logger.new(STDERR)
+	end
+end
+
+include Logging
+
+logger.info("Hello World from AthensLister logger!")
